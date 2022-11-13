90aa8138f17c7a7df00787331d7466619c07ac5a
PABLOLDA <115592448+PABLOLDA@users.noreply.github.com>
2022-11-13 12:18:16 -0300

    Add files via upload
    calculadora con java

---
+++ java calculadora.js
@@ -0,0 +1,29 @@
+numero1=10
+numero2=3
+
+let calculadora = "/"
+    switch(calculadora){
+        case "*":
+            calculadora = numero1*numero2
+            break
+        case "/" :
+            calculadora = numero1/numero2
+            break
+        case "+": 
+        calculadora = numero1+numero2
+            break
+        case "-":
+            calculadora = numero1-numero2
+            break
+        default:(error)
+    }
+
+numero1=2
+numero2=1
+operacion="+"
+console.log(calculadora)
+
+
+
+
+
